ce6c000f9550967c26176a7dda9eb68872087b35
nikitarub <nikita.rub@yandex.ru>
2019-06-23 19:19:04 +0300

    #6: Modified tests with uuid in block

---
+++ pkg/tests/runBench.sh
@@ -0,0 +1,1 @@
+go test -bench . -benchmem -count 4 -cpu 4 -benchtime 3s
--- pkg/tests/storages_test.go
+++ pkg/tests/storages_test.go
@@ -1,1 +1,45 @@
 package tests
+
+import (
+	"testing"
+
+	"tp-highload-performance-test/pkg/models"
+	"tp-highload-performance-test/pkg/storages/leveldb"
+
+	"github.com/google/uuid"
+)
+
+const (
+	NBlocks      = 100
+	NConnections = 1
+	NBatch       = 256
+)
+
+func BenchmarkConnectionsWrite(b *testing.B) {
+	id, err := uuid.Parse("00000000-0000-0000-0000-000000000001")
+	if err != nil {
+		panic(err)
+	}
+	blocks := generateBlocks(NBlocks, models.UUID(id), NBatch)
+	b.ResetTimer()
+
+	repo := leveldb.NewRepository()
+	err = repo.OpenConnection()
+
+	if err != nil {
+		panic("Failed to connect")
+	}
+	defer repo.CloseConnection()
+
+	for i := 0; i < NBlocks; i++ {
+		err = repo.SaveBlock(blocks[i])
+		if err != nil {
+			panic("Failed to save block")
+		}
+	}
+}
+
+func BenchmarkGenerateBlocks(b *testing.B) {
+	id := uuid.MustParse("00000000-0000-0000-0000-000000000001")
+	generateBlocks(NBlocks, models.UUID(id), 256)
+}
